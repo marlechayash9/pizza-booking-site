bc7848083fcafbabc94922a0f513d7ccadbb5905
Ishaan Lagwankar <ishaanlagwankar@gmail.com>
2018-11-17 14:34:27 +0530

    alert given

--- addtocart.js
+++ addtocart.js
@@ -6,12 +6,16 @@ function add(idclick) {
     switch(idclick)
     {
         case "Pepperoni" : sessionStorage.setItem(a,"Pepperoni");
+                            alert("Added!");
                             break;
         case "mag" : sessionStorage.setItem(a,"Margherita");
+                    alert("Added!");
                     break;
         case "4Cheese" : sessionStorage.setItem(a,"4 Cheese");
+                    alert("Added!");
                     break;
         case "tuna" : sessionStorage.setItem(a,"Tuna");
+                    alert("Added!");
                     break;
     }
     a = parseInt(a);
